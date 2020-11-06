228b24b83a22a8da7eea84fb410ec2a8f3de2df7
Joseph Shuttleworth <pmyjgs@nottingham.ac.uk>
2020-11-06 15:37:21 +0000

    Update plot_extrapolation script. Add plots

--- plot_extrapolation.py
+++ plot_extrapolation.py
@@ -3,6 +3,9 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib.style
+import csv
+import os.path
+import numpy as np
 
 matplotlib.style.use('classic')
 
@@ -10,6 +13,15 @@ def make_plot(model_name):
     numeric_dir = "~/extrapolation_data_numeric/"
     dir = "~/extrapolation_data/"
 
+    with open(os.path.expanduser('~/extrapolation_data/') + model_name + "/500JumpParameters.dat") as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=' ')
+        lines = [row for row in csv_reader]
+
+    jump_pace = int(lines[0][0])
+    buffer_size = int(lines[0][1])
+    extrapolation_coefficient = float(lines[0][2])
+
+    extrap_parameters = [[float(cell) for cell in line] for line in lines[1:]]
 
     dir = dir + model_name + "/TestExtrapolation/"
     numeric_dir = numeric_dir + model_name + "/TestExtrapolation/"
@@ -21,18 +33,40 @@ def make_plot(model_name):
     brute_paces = brute_data['pace']
     smart_paces = smart_data['pace']
 
-    for state_var in smart_data:
+    line_no = 0
+    for state_var in smart_data.drop(['pace'], axis=1):
+        if line_no == len(extrap_parameters):
+            break
         smart_var = smart_data[state_var]
         brute_var = brute_data[state_var]
 
+        state_no = int(extrap_parameters[line_no][0])
+        tau = -1/extrap_parameters[line_no][1]
+        beta = -1/tau
+        alpha = extrap_parameters[line_no][2]
+
+        V_diff = np.exp(alpha  - buffer_size/tau)/(1 - np.exp(-1/tau));
+
+
+        if smart_var.values[jump_pace-1] - smart_var.values[jump_pace - buffer_size-1] < 0:
+            V_diff = -V_diff;
+
+        V = brute_var[jump_pace-1] + V_diff*(1 - np.exp(-(np.array(range(-jump_pace+1, len(brute_var) - jump_pace + 1))/tau)))
+
         plt.plot(smart_paces, smart_var)
         plt.plot(brute_paces, brute_var)
-        # plt.axhline(smart_var.values[-1], c="burlywood", linestyle='--')
-        # plt.axhline(brute_var.values[-1], c="burlywood", linestyle='--')
+        plt.plot(range(0 ,len(brute_data)), V, linestyle = '--', color="orange")
         plt.axhline(numeric_brute_data[state_var].values[-1], linestyle ='--')
-        # plt.legend(["Extrapolation Method", "Brute Force Method", "Terminal value from extrapolation method", "Terminal value from brute force method"])
+        plt.axhline(smart_var[jump_pace-2] + V_diff, color="grey", linestyle = "-.")
+
+        plt.plot(jump_pace, smart_var[jump_pace-1] + V_diff, label="point", markersize=5, color='red', marker='x')
+        plt.legend(["extrapolation method solution", "brute force solution", "fitted curve", "Terminal value from brute force method on numeric model", "Predicted terminal value from fitted curve", "Point extrapolated to"], prop={'size': 5})
         plt.title(model_name + " " + state_var)
-        plt.show()
+        if not os.path.exists("data"):
+            os.mkdir("data")
+        plt.savefig("data/"+model_name+"_"+state_var+".png")
+        line_no += 1
 
 if __name__=="__main__":
     make_plot("ohara_rudy_cipa_v1_2017")
+    make_plot("tentusscher_model_2006_epi")
