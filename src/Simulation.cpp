9cbec72e8bd8856710aa8baeef62456db13e0791
Joseph Shuttleworth <pmyjgs@nottingham.ac.uk>
2021-01-11 14:44:27 +0000

    Add plots to plots/. Add Simulation.cpp.

---
+++ src/Simulation.cpp
@@ -0,0 +1,93 @@
+#include "Simulation.hpp"
+
+Simulation::Simulation(boost::shared_ptr<AbstractCvodeCell> _p_model, double _period, std::string input_path, double _tol_abs, double _tol_rel) : mpModel(_p_model), mPeriod(_period), mTolAbs(_tol_abs), mTolRel(_tol_rel){
+  mFinished = false;
+  mpStimulus = mpModel->UseCellMLDefaultStimulus();
+  mpStimulus->SetStartTime(0);
+  //There's no need to be near the second stimulus because Solve is called for
+  //each pace
+  mpStimulus->SetPeriod(2*mPeriod);
+  mpModel->SetMaxSteps(1e5);
+  mpModel->SetMaxTimestep(1000);
+  mpModel->SetTolerances(mTolAbs, mTolRel);
+  mpModel->SetMinimalReset(false); //Not sure if this is needed
+  mNumberOfStateVariables = mpModel->GetSystemInformation()->rGetStateVariableNames().size();
+  mStateVariables = mpModel->GetStdVecStateVariables();
+  if(input_path.length()>=1){
+    LoadStatesFromFile(mpModel, input_path);
+  }
+}
+
+
+bool Simulation::RunPaces(int max_paces){
+  for(int i = 0; i <= max_paces; i++){
+    if(RunPace())
+      return true;
+  }
+  return false;
+}
+
+bool Simulation::RunPace(){
+  if(mFinished)
+    return false;
+  /*Solve in two parts*/
+  std::vector<double> tmp_state_variables = mpModel->GetStdVecStateVariables();
+  // mpModel->SetForceReset(true);
+  mpModel->SolveAndUpdateState(0, mpStimulus->GetDuration());
+  mpStimulus->SetPeriod(mPeriod*2);
+  mpModel->SolveAndUpdateState(mpStimulus->GetDuration(), mPeriod);
+  mpStimulus->SetPeriod(mPeriod);
+  std::vector<double> new_state_variables = mpModel->GetStdVecStateVariables();
+  mCurrentMrms = mrms(tmp_state_variables, new_state_variables);
+  mpModel->SetStateVariables(new_state_variables);
+  if(mCurrentMrms < mThreshold){
+    mFinished = true;
+    return true;
+  }
+  return false;
+}
+
+OdeSolution Simulation::WritePaceToFile(std::string dirname, std::string filename, double sampling_timestep){
+  OdeSolution solution = mpModel->Compute(0, mPeriod, 1);
+  solution.WriteToFile(dirname, filename, "ms");
+  return solution;
+}
+
+double Simulation::GetMrms(){
+  if(mFinished)
+    return NAN;
+  else
+    return mCurrentMrms;
+}
+
+bool Simulation::IsFinished(){
+  return mFinished;
+}
+
+OdeSolution Simulation::GetPace(){
+  return mpModel->Compute(0, mPeriod, mSamplingTimestep);
+}
+
+std::vector<double> Simulation::GetStateVariables(){
+  return mpModel->GetStdVecStateVariables();
+}
+
+void Simulation::SetStateVariables(std::vector<double> states){
+  mpModel->SetStateVariables(states);
+}
+
+void Simulation::SetThreshold(double threshold){
+  /* The threshold must be non-negative */
+  if(threshold<0){
+    throw std::exception();
+  }
+  mThreshold = threshold;
+}
+
+double Simulation::GetApd(double percentage){
+  mpStimulus->SetPeriod(mPeriod);
+  OdeSolution solution = mpModel->Compute(mPeriod-10, 2*mPeriod, 1);
+  solution.CalculateDerivedQuantitiesAndParameters(mpModel.get());
+  CellProperties cell_props(solution.GetAnyVariable("membrane_voltage"), solution.rGetTimes());
+  return cell_props.GetLastActionPotentialDuration(90);
+}
