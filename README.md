60673e10bbabb3af0da7709536c64c7bccf65fd9
Joseph Shuttleworth <pmyjgs@nottingham.ac.uk>
2021-01-11 14:01:29 +0000

    Delete old files. Edit Readme.

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
-# A template user project for use with Chaste.
+# Extrapolation To Steady State Project
 
-When adding a test its filename must be added to the file ContinuousTestPack.txt
+## Purpose
+This project investigates the detection and computation of limit cycles in single cell cardiomyocyte models in Chaste. TestErrorMeasures.hpp compares several candidate error measures for limit cycle detection and the class SmartSimulation contains a method to extrapolate to these limit cycles.
+
+## Building and Running
+It is recommended to use Docker to install Chaste and its dependencies. See https://github.com/Chaste/chaste-docker for more details.
+
+Then, clone this repository into Chaste's projects folder (~/projects or ~/Chaste/projects/ if you're using the docker image).
+
+Next, build Chaste by following these instructions https://chaste.cs.ox.ac.uk/trac/wiki/ChasteGuides/UserProjects .
+
+You are now ready to run the project (and the Continuous test pack) with ```ctest -j4 Continuous -V```. This will produce output in /tmp/Chaste which can be copied and saved. plot_extrapolation.py assumes that /tmp/chaste/ has been copied to ~/Chaste/data/ and produces the plots in the plots folder.
--- test/TestGroundTruthSimulation.hpp
+++ test/TestGroundTruthSimulation.hpp
@@ -52,7 +52,17 @@ OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include "ohara_rudy_2011_endoCvode.hpp"
 #include "shannon_wang_puglisi_weber_bers_2004Cvode.hpp"
 #include "decker_2009Cvode.hpp"
+#include "ohara_rudy_cipa_v1_2017Cvode.hpp"
+#include "ohara_rudy_cipa_v1_2017_analyticCvode.hpp"
+#include "ten_tusscher_model_2006_epiCvode.hpp"
+#include "ten_tusscher_model_2006_epi_analyticCvode.hpp"
 
+/* Run the models under different scenarios and output:
+   - All variables over the final pace
+   - The APD90
+   - Terminal state variables
+   to separate files.
+ */
 class TestGroundTruthSimulation : public CxxTest::TestSuite
 {
 public:
@@ -63,98 +73,43 @@ public:
     boost::shared_ptr<AbstractIvpOdeSolver> p_solver;
 
     std::vector<boost::shared_ptr<AbstractCvodeCell>> models;
-    
+
     models.push_back(boost::shared_ptr<AbstractCvodeCell>(new Cellohara_rudy_2011_endoFromCellMLCvode(p_solver, p_stimulus)));
     models.push_back(boost::shared_ptr<AbstractCvodeCell>(new Celldecker_2009FromCellMLCvode(p_solver, p_stimulus)));
     models.push_back(boost::shared_ptr<AbstractCvodeCell>(new Cellten_tusscher_model_2004_epiFromCellMLCvode(p_solver, p_stimulus)));
     models.push_back(boost::shared_ptr<AbstractCvodeCell>(new Cellshannon_wang_puglisi_weber_bers_2004FromCellMLCvode(p_solver, p_stimulus)));
-    models.push_back(boost::shared_ptr<AbstractCvodeCell>(new Cellohara_rudy_2011_endoFromCellMLCvode(p_solver, p_stimulus)));
-    models.push_back(boost::shared_ptr<AbstractCvodeCell>(new Celldecker_2009FromCellMLCvode(p_solver, p_stimulus)));
-    models.push_back(boost::shared_ptr<AbstractCvodeCell>(new Cellten_tusscher_model_2004_epiFromCellMLCvode(p_solver, p_stimulus)));
-    models.push_back(boost::shared_ptr<AbstractCvodeCell>(new Cellshannon_wang_puglisi_weber_bers_2004FromCellMLCvode(p_solver, p_stimulus)));
+    models.push_back(boost::shared_ptr<AbstractCvodeCell>(new Cellten_tusscher_model_2006_epi_analyticCvodeFromCellMLCvode(p_solver, p_stimulus)));
+    models.push_back(boost::shared_ptr<AbstractCvodeCell>(new Cellohara_rudy_cipa_v1_2017_analyticCvodeFromCellMLCvode(p_solver, p_stimulus)));
 
     std::string username = std::string(getenv("USER"));
     boost::filesystem::create_directory("/tmp/"+username);
-    
-    for(unsigned int i = 0; i < 8; i++){
-      double period = 1000;
-      if(i<1)
-	period = 500;
 
-      boost::shared_ptr<AbstractCvodeCell> p_model = models[i];
-      boost::shared_ptr<RegularStimulus> p_regular_stim = p_model->UseCellMLDefaultStimulus();
-      
+    for(unsigned int i = 0; i < models.size(); i++){
       const std::string model_name = p_model->GetSystemInformation()->GetSystemName();
       boost::filesystem::create_directory("/tmp/"+username+"/"+model_name);
-      boost::filesystem::create_directory("/tmp/"+username+"/"+model_name+"/GroundTruth2Hz");
-      boost::filesystem::create_directory("/tmp/"+username+"/"+model_name+"/GroundTruth1Hz");
-      const double duration   = p_regular_stim->GetDuration();
+      std::vector<double> periods = {500, 1000};
+      std::vector<std::vector<double>> initial_states = models[i]->GetStdVecStateVariables();
+      for(j =0; j < periods.size(); j++){
+        const std::string dirname = "/tmp/"+username+"/"+model_name+"/TestGroundTruth" + periods[4] + "ms/";
+        boost::filesystem::create_directory(dirname);
+        // Initialise simulation with fine tolerances
+        Simulation simulation(models[i], periods[j], "", 1e-12, 1e-12);
+        simulation->SetStateVariables(initial_states);
 
-      p_regular_stim->SetPeriod(period);
-      p_regular_stim->SetStartTime(0);
-      p_model->SetTolerances(1e-12, 1e-12);
-      p_model->SetMaxSteps(1e5);
-      
-      double sampling_timestep = 0.01;
-      const unsigned int paces  = 10000;
-      OdeSolution current_solution;
-      std::ofstream apd_file;
-      std::ofstream variables_file;
-      const std::vector<std::string> state_variable_names = p_model->rGetStateVariableNames();
-
-      std::cout << "Testing " << model_name << " with period " << period << "\n";
-      p_model->SetMaxTimestep(1000);
-      if(period==500){
-	apd_file.open("/tmp/"+username+"/"+model_name+"/GroundTruth2Hz/apd90.dat");
-	variables_file.open("/tmp/"+username+"/"+model_name+"/GroundTruth2Hz/final_state_variables.dat");
+        // Turn off convergence criteria
+        simulation.SetThreshold(0);
+        // Run the simulation for a large number of paces
+        simulation.RunPaces(1e4);
+        // Output the final pace
+        const pace_filename = "finalpace.dat";
+        simulation.WritePaceToFile(dirname, pace_filename);
+        // Output the APD90 of the final pace
+        const apd_filename = "final_apd90.dat";
+        std::ofstream apd_file(dirname + apd_filename);
+        apd_file << simulation.GetApd(90) << "\n";
+        apd_file.close();
       }
-      else{
-	apd_file.open("/tmp/"+username+"/"+model_name+"/GroundTruth1Hz/apd90.dat");
-	variables_file.open("/tmp/"+username+"/"+model_name+"/GroundTruth1Hz/final_state_variables.dat");
-      }
-      TS_ASSERT_EQUALS(apd_file.is_open(), true);
-      TS_ASSERT_EQUALS(variables_file.is_open(), true);
-    /*Set the output to be as precise as possible */
-    apd_file.precision(18);
-    variables_file.precision(18);
-
-    /*Run the simulation*/
-    for(unsigned int i = 0; i < paces - 1; i++){
-      /*Set the initial values to be the terminal values of the last solution*/
-      p_model->SolveAndUpdateState(0, duration);
-      p_model->SolveAndUpdateState(duration, period);
-    }
-    
-    std::vector<std::vector<double>> final_trace;
-    std::vector<double> times;
-
-    /*Store the solution to the final pace and create std::vectors of the voltages and the times*/
-    current_solution = p_model->Compute(0, duration, sampling_timestep);
-    times = current_solution.rGetTimes();
-    final_trace = current_solution.rGetSolutions();
-    current_solution = p_model->Compute(duration, period, sampling_timestep);
-    times.insert(times.end(), ++current_solution.rGetTimes().begin(), current_solution.rGetTimes().end());
-    final_trace.insert(final_trace.end(), ++current_solution.rGetSolutions().begin(), current_solution.rGetSolutions().end());
-	
-    unsigned int voltage_index = p_model->GetSystemInformation()->GetStateVariableIndex("membrane_voltage");
-
-    std::vector<double> voltages = GetNthVariable(final_trace, voltage_index);
-    
-    /*Output terminal state variables to file*/
-    for(unsigned int i = 0; i < final_trace[0].size(); i++){
-      variables_file << final_trace.back()[i] << " ";
-    }
-    variables_file << "\n";
-	
-    /*Calculate and output APD90 to file*/
-    CellProperties cell_props = CellProperties(voltages, times); 
-    double apd = cell_props.GetLastActionPotentialDuration(90);
-    variables_file.close();
-    apd_file << apd << " ";
-    std::cout << model_name << " " << apd << " " << period << "\n";
-    apd_file << "\n";
-    apd_file.close();	
-    }
+   }
 #else
     std::cout << "Cvode is not enabled.\n";
 #endif
