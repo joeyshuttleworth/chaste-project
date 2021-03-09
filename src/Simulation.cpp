d2be48346aab76d0f835ee23cb39b3e32c378b43
Joseph Shuttleworth <pmyjgs@nottingham.ac.uk>
2021-03-09 17:21:01 +0000

    Fix typos

--- src/Simulation.cpp
+++ src/Simulation.cpp
@@ -182,10 +182,10 @@ void Simulation::SetIKrBlock(double block){
 
   const std::vector<std::string> parameter_names = mpModel->rGetParameterNames();
 
-  if(std::count_if(parameter_names.begin(), parameter_names.end(), [&](std::string name) -> bool {retur name==GKrParameterName}) > 0){
+  if(std::count_if(parameter_names.begin(), parameter_names.end(), [&](std::string name) -> bool {retur name==GKr_parameter_name}) > 0){
     // Parameter exists
     if(mDefaultGKr == DOUBLE_UNSET)
-      mDefaultGKr = mpModel->GetParameter(GKrParameterName);
+      mDefaultGKr = mpModel->GetParameter(GKr_parameter_name);
     mpModel->SetParameter(GKr_parameter_name, mDefaultGKr*(1-block));
     return;
   }
