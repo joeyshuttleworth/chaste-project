f6e34356c81eaf1c280cb8150aafaa96a48c4581
Joseph Shuttleworth <pmyjgs@nottingham.ac.uk>
2021-04-28 13:40:41 +0100

    Added boxplots

--- scripts/benchmark_processing.py
+++ scripts/benchmark_processing.py
@@ -1,8 +1,11 @@
+#!/usr/bin/env python3
+
 import os
 import re
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
+import seaborn as sns
 
 def summarise_model(model_name):
     file_path = os.path.join(os.path.join(os.path.expanduser("~"), "Chaste-from-server", "testoutput", "TestBenchmark", "{}_results.dat".format(model_name)))
@@ -26,30 +29,84 @@ def summarise_model(model_name):
 
     total_scores = []
 
+    # Get reference rows
+    print(df_results.columns)
+    reference_df=df_results[df_results.extrapolation_constant==0]
+    reference_df=reference_df[reference_df.buffer_size==100]
+    reference_df=reference_df[["score", "ic_period", "ic_block", "period", "IKrBlock"]]
+
+    # Add fraction paces saved column
+    reference_df=reference_df.set_index(["ic_period", "ic_block", "period", "IKrBlock"])
+    normalised_paces_saved_list=[]
     # print scores for each set of parameters
     for e_c in extrapolation_constants:
         for buffer_size in buffer_sizes:
             rows = df_results[df_results.extrapolation_constant == e_c]
             rows = rows[rows.buffer_size == buffer_size]
+
+            rows = rows.set_index(["ic_period", "ic_block", "period", "IKrBlock"])
+
             scores = rows["score"].values
+
+            paces_saved = (reference_df- rows)["score"]
+            normalised_paces_saved = ((reference_df- rows)/reference_df)["score"]
+            normalised_paces_saved_list.append([buffer_size, e_c, normalised_paces_saved])
+
             number_of_scenarios = len(scores)
             total_score = sum(scores)
             jumps_used = sum(rows["jumps_used"])
-            total_scores.append([e_c, int(buffer_size), jumps_used, total_score])
+            total_scores.append([e_c, int(buffer_size), jumps_used, total_score, total_score/sum(reference_df["score"]), normalised_paces_saved.quantile(0), normalised_paces_saved.quantile(0.25), normalised_paces_saved.quantile(0.5), normalised_paces_saved.quantile(0.75), normalised_paces_saved.quantile(1), paces_saved.mean()])
 
-    total_scores = pd.DataFrame(total_scores, columns=["lmbda", "n", "jumps used", "paces_saved"])
+    total_scores = pd.DataFrame(total_scores, columns=["lmbda", "n", "jumps used", "paces_saved", "normalised_paces_saved", "min", "lq", "median", "uq", "max", "mean"])
 
     total_scores = total_scores[total_scores.paces_saved != 0]
-    # Find reference result - this was given by n=100, lambda=0
-    row=total_scores[total_scores.lmbda==0]
-    row=row[total_scores.n==100]
-    reference_score = row["paces_saved"].values[0]
+
+    reference_score = total_scores[total_scores.lmbda==0]
+    reference_score = reference_score[reference_score.n==100]["paces_saved"].values[0]
     print("reference_score {}".format(reference_score))
+
+    total_scores["percentage_paces_saved"] = (100*(reference_score - total_scores["paces_saved"])/reference_score).astype(int)
 
     total_scores["paces_saved"] = reference_score - total_scores["paces_saved"]
 
-    total_scores.sort_values(by=['paces_saved'], inplace=True)
-    print(total_scores)
+    total_scores.sort_values(by=['paces_saved'], inplace=True, ascending=False)
+    # plot boxplots for the top 3 scores
+    scores_to_plot=total_scores.iloc[0:8]
+
+    df_to_plot=[]
+    column_names=[]
+    for [index, settings] in scores_to_plot.iterrows():
+        lmbda = settings["lmbda"]
+        n    = settings["n"]
+        # setting_results = df_results[df_results.buffer_size==n]
+        # setting_results = setting_results[setting_results.extrapolation_constant==lmda]["normalised_paces_saved"].values
+
+        setting_results=next(filter(lambda x: x[0]==n and x[1] == lmbda, normalised_paces_saved_list))[2]
+
+        if(len(setting_results)!=36):
+            print(len(setting_results), n, lmbda)
+        column_names.append("n={}, lamda={}".format(n,lmbda))
+        df_to_plot.append(setting_results)
+
+    df_to_plot=np.stack(df_to_plot).transpose()
+    df_to_plot = pd.DataFrame(df_to_plot, columns=column_names)
+
+
+
+    # make boxplot with Seaborn
+    bplot=sns.boxplot(
+                 data=df_to_plot,
+                 width=0.5,
+                 palette="colorblind")
+
+    # add stripplot to boxplot with Seaborn
+    bplot=sns.stripplot(data=df_to_plot,
+                   jitter=True,
+                   marker='o',
+                   alpha=0.5,
+                   color='black')
+
+    plt.show()
 
     max_final_mrms=0
     max_apd_range=0
@@ -67,7 +124,7 @@ def summarise_model(model_name):
             mrms_errors = rows["last_mrms"].values
             max_final_mrms = max([max(mrms_errors), max_final_mrms])
 
-    print("biggest final mrms was ".format(max_final_mrms))
+    print("biggest final mrms was {}".format(max_final_mrms))
     print("max apd range was {}".format(max_apd_range))
 
 if __name__=="__main__":
